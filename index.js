5a7e1172dcec27e56cd602a6b63dd4637af8cdc1
Syed Raza <syed.raza011@gmail.com>
2023-01-09 12:12:15 -0500

    baics are done

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // alert("hello");
+const data = [
+  {
+    address: ["1235, Edgar Rice"],
+    PropertyType: "com",
+    city: ["Long island"],
+    sate: "New York",
+  },
+  {
+    address: ["bnson ave , riggs street "],
+    PropertyType: "com",
+    city: ["Canal Street "],
+    sate: "New York",
+  },
+  {
+    address: ["1235, Edgar Rice"],
+    PropertyType: "res",
+    city: ["brooklyn"],
+    sate: "New York",
+  },
+  {
+    address: ["1235, calendron street"],
+    PropertyType: "com",
+    city: ["Manhattan"],
+    sate: "China Town",
+  },
+  {
+    address: ["10012 emmons, Edgar Rice street "],
+    PropertyType: "res",
+    city: ["queens"],
+    sate: "New York",
+  },
+  {
+    address: ["bay pkwy, sutter ave"],
+    PropertyType: "store",
+    city: ["brooklyn"],
+    sate: "michigan",
+  },
+];
