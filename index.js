dbd1e651280cc62b630fa716df32e5763f008f5c
Syed Raza <syed.raza011@gmail.com>
2023-06-15 10:48:45 -0400

    update

--- index.js
+++ index.js
@@ -1,57 +1,25 @@
-// // alert("hello");
-// const data = [
-//   {
-//     address: ["1235, Edgar Rice"],
-//     PropertyType: "com",
-//     city: ["Long island"],
-//     sate: "New York",
-//   },
-//   {
-//     address: ["bnson ave , riggs street "],
-//     PropertyType: "com",
-//     city: ["Canal Street "],
-//     sate: "New York",
-//   },
-//   {
-//     address: ["1235, Edgar Rice"],
-//     PropertyType: "res",
-//     city: ["brooklyn"],
-//     sate: "New York",
-//   },
-//   {
-//     address: ["1235, calendron street"],
-//     PropertyType: "com",
-//     city: ["Manhattan"],
-//     sate: "China Town",
-//   },
-//   {
-//     address: ["10012 emmons, Edgar Rice street "],
-//     PropertyType: "res",
-//     city: ["queens"],
-//     sate: "New York",
-//   },
-//   {
-//     address: ["bay pkwy, sutter ave"],
-//     PropertyType: "store",
-//     city: ["brooklyn"],
-//     sate: "michigan",
-//   },
-// ];
 class realEstateP {
   constructor(address, propertyType, city, state) {
     this.address = address;
-    this.PropertyType = propertyType;
+    this.propertyType = propertyType;
     this.city = city;
     this.state = state;
   }
 }
-let submit_btn=document.querySelector("button")
+let submit_btn=document.getElementById("button")
 submit_btn.addEventListener("click", ()=>{
+    console.log(submit_btn);
     let address = document.querySelector("#address");
-    let PropertyType = document.querySelector("#propertyType");
+    console.log(address)
+    let propertyType = document.querySelector("#propertyType");
     let city = document.querySelector("#city");
     let state = document.querySelector("#state");
-    let container=document.createElement("div")
+    let container=document.createElement("ul")
+    let insdieConatiner=document.createElement("li")
+    insdieConatiner.append(address)
+    // ,propertyType,city,state
+    container.innerHTML=insdieConatiner
+    console.log(container)
 })
 
 
